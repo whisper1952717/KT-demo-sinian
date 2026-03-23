1a0ea68ea56d3bd1151dc6e06dd3375cfc59c8b4
whisper1952717 <whisper1952717@users.noreply.github.com>
2026-03-23 22:27:44 +0800

    chore: make routing and base path compatible with PinMe

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@xyflow/react/dist/style.css";
@@ -14,9 +14,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ConfigProvider theme={antdTheme}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ConfigProvider>
     </QueryClientProvider>
   </StrictMode>,
